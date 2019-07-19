592917d6ca036e842464fdad7b3ca03deba37cb5
Konrad <konradsmail@me.com>
2019-07-19 18:04:21 +0200

    lustiges coden

--- getting_started.py
+++ getting_started.py
@@ -1,1 +1,22 @@
-print('Hello world')
+from random import choice
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Hello {name}'.format(name=self.name)
+
+def main():
+    people = [
+        Person('Jane'),
+        Person('Jill'),
+        Person('Konrad')
+    ]
+    person = choice(people)
+    print(person)
+
+
+if __name__ == '__main__':
+    main()
